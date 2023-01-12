057b6c6e949b25ab7818df93b2e1e88ae0d898cb
William Corotan <williamcorotan@gmail.com>
2023-01-12 21:34:00 +0800

    edited for review session

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,3 @@
-
 const  mongoose  = require('mongoose')
 const Tour = require('../models/tourModel')
 
@@ -6,121 +5,118 @@ const Tour = require('../models/tourModel')
 
 //function
 exports.getAllTours = async (req,res)=>{
-  try{
-    const tours = await Tour.find()
-    res.status(201).json(
-      {
-      status: 'success', 
-      data: {
-        Tours: tours
-    }})
-  }
-  catch(err){
-    res.status(400).json(
-      {
-        status: 'failed',
-        data: {
-          Error: err
-        }
-      }
-    )
-  }
+  // try{
+  //   const tours = await Tour.find()
+  //   res.status(201).json(
+  //     {
+  //     status: 'success', 
+  //     data: {
+  //       Tours: tours
+  //   }})
+  // }
+  // catch(err){
+  //   res.status(400).json(
+  //     {
+  //       status: 'failed',
+  //       data: {
+  //         Error: err
+  //       }
+  //     }
+  //   )
+  // }
 }
 
 exports.getTour = async (req,res)=>{
-  try{
-    const tour = await Tour.findById(req.params.id)
+  // try{
+  //   const tour = await Tour.findById(req.params.id)
 
-    res.status(201).json(
-      {
-      status: 'success', 
-      data: {
-        tour
-    }})
-  }
-  catch(err){
-    res.status(400).json(
-      {
-        status: 'failed',
-        data: {
-          Error: err
-        }
-      }
-    )
-  }
+  //   res.status(201).json(
+  //     {
+  //     status: 'success', 
+  //     data: {
+  //       tour
+  //   }})
+  // }
+  // catch(err){
+  //   res.status(400).json(
+  //     {
+  //       status: 'failed',
+  //       data: {
+  //         Error: err
+  //       }
+  //     }
+  //   )
+  // }
 }
 
 exports.createTour =  async (req,res)=>{  
- 
-  try{
-    const newTour = await Tour.create(req.body)
+  // try{
+  //   const newTour = await Tour.create(req.body)
 
-    res.status(201).json(
-      {
-        status: 'added',
-        data: {
-          tour: newTour
-        }
-      }
-    )
-  }catch(err){
-    res.status(400).json(
-      {
-        status: 'failed',
-        data: {
-          Error: err
-        }
-      }
-    )
-  }
+  //   res.status(201).json(
+  //     {
+  //       status: 'added',
+  //       data: {
+  //         tour: newTour
+  //       }
+  //     }
+  //   )
+  // }catch(err){
+  //   res.status(400).json(
+  //     {
+  //       status: 'failed',
+  //       data: {
+  //         Error: err
+  //       }
+  //     }
+  //   )
+  // }
   }
 
 exports.updateTour = async (req,res)=>{
-  try{
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
-    res.status(200).json(
-      {
-        status: 'Updated',
-        data: {
-          tour
-        }
-      }
-    )
-  }
-  catch(err){
-    res.status(400).json(
-      {
-        status: 'failed',
-        data: {
-          Error: err
-        }
-      }
-    )
-  }
-
+  // try{
+  //   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+  //   res.status(200).json(
+  //     {
+  //       status: 'Updated',
+  //       data: {
+  //         tour
+  //       }
+  //     }
+  //   )
+  // }
+  // catch(err){
+  //   res.status(400).json(
+  //     {
+  //       status: 'failed',
+  //       data: {
+  //         Error: err
+  //       }
+  //     }
+  //   )
+  // }
 }
 
 exports.deleteTour = async (req, res) =>{
-  try{
-    await Tour.findByIdAndDelete(req.params.id,)
-    res.status(204).json(
-      {
-        status: 'Deleted',
-        data: {
-          message: 'Deleted Entry'
-        }
-      }
-    )
-  }
-  catch(err){
-    res.status(400).json(
-      {
-        status: 'failed',
-        data: {
-          Error: err
-        }
-      }
-    )
-  }
-
+  // try{
+  //   await Tour.findByIdAndDelete(req.params.id,)
+  //   res.status(204).json(
+  //     {
+  //       status: 'Deleted',
+  //       data: {
+  //         message: 'Deleted Entry'
+  //       }
+  //     }
+  //   )
+  // }
+  // catch(err){
+  //   res.status(400).json(
+  //     {
+  //       status: 'failed',
+  //       data: {
+  //         Error: err
+  //       }
+  //     }
+  //   )
+  // }
 }
--- server.js
+++ server.js
@@ -1,34 +1,7 @@
 const dotenv = require('dotenv')
-const mongoose = require('mongoose')
 dotenv.config({path: './config.env'})
 
 const app = require('./app')
-
-const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD) 
-
-const DB_LOCAL = process.env.DATABASE_LOCAL
-
-//CONNECTION TO CLOUD DATABASE
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then((con)=>{
-console.log("Database Connected")
-}).catch((err)=>{
-  console.log(err)
-})
-
-// CONNECTION TO LOCAL DATABASE
-// mongoose.connect(DB_LOCAL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then((con)=>{
-// console.log(con.connections)
-// console.log("Database Connected")
-// }).catch((err)=>{
-//   console.log(err)
-// })
-
 
 //SERVER
 const PORT = process.env.PORT || 8000
